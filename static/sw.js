34f7445140c95abc0ca7e3b08dc1c3977aae3d90
compasssites <mywebsite@mycompass.in>
2026-02-01 19:05:27 +0530

    Fix LTP overlap with wider columns

--- static/sw.js
+++ static/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "option-chain-v8";
+const CACHE_NAME = "option-chain-v9";
 const STATIC_ASSETS = [
   "/",
-  "/static/styles.css?v=20260201g",
-  "/static/app.js?v=20260201g",
+  "/static/styles.css?v=20260201h",
+  "/static/app.js?v=20260201h",
   "/manifest.webmanifest",
   "/static/icons/icon-192.svg",
   "/static/icons/icon-512.svg",
