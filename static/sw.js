191dd9b27d31215776c8d4819f14cf73d6ef0284
compasssites <mywebsite@mycompass.in>
2026-02-01 19:57:48 +0530

    Stabilize mobile scroll and fix header overlap

--- static/sw.js
+++ static/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "option-chain-v9";
+const CACHE_NAME = "option-chain-v10";
 const STATIC_ASSETS = [
   "/",
-  "/static/styles.css?v=20260201h",
-  "/static/app.js?v=20260201h",
+  "/static/styles.css?v=20260201i",
+  "/static/app.js?v=20260201i",
   "/manifest.webmanifest",
   "/static/icons/icon-192.svg",
   "/static/icons/icon-512.svg",
