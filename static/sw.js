15acda2a55c4adf154f662f6cdfda1f23dea6b80
compasssites <mywebsite@mycompass.in>
2026-02-06 07:25:15 +0530

    Expand metals widgets and move status into table

--- static/sw.js
+++ static/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "option-chain-v16";
+const CACHE_NAME = "option-chain-v17";
 const STATIC_ASSETS = [
   "/",
-  "/static/styles.css?v=20260205f",
-  "/static/app.js?v=20260205f",
+  "/static/styles.css?v=20260205g",
+  "/static/app.js?v=20260205g",
   "/manifest.webmanifest",
   "/static/icons/icon-192.svg",
   "/static/icons/icon-512.svg",
